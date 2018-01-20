08880116a86e5091a0676933bdeba2e15f160366
Igor Liferenko <igor.liferenko@gmail.com>
2018-01-20 14:33:07 +0700

    edit

--- stereo.c
+++ stereo.c
@@ -1,30 +1,30 @@
-#include <unistd.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <stdio.h> /* |BUFSIZ| */
-#include <fcntl.h>
-
-#define MIN(X,Y) ((X) < (Y) ? (X) : (Y))
+#include <stdio.h>
 
 int main(int argc, char **argv)
 {
-  struct stat st;
-  char buf[BUFSIZ];
-  int fd_in = open(argv[1], O_RDONLY);
-  fstat(fd_in, &st);
-  int fd_out = open("out.dat", O_CREAT | O_WRONLY, st.st_mode);
-  size_t remaining = (size_t) st.st_size;
-  while (remaining > 0) {
-    size_t requested = MIN(sizeof buf, remaining);
-    ssize_t rcount = read(fd_in, buf, requested);
-    if (rcount < 0) return 1;
-    for (int i = 0; i < rcount; i++) {
-      ssize_t wcount = write(fd_out, buf+i, 1);
-      if (wcount < 0 || wcount == 0 || (size_t)rcount < requested) return 1;
-      remaining -= 1;
-      if (i%2 == 0 && i != 0) write(fd_out, "\0\0", 2);
+  (void) argc;
+  FILE *fp_in, *fp_out;
+  if ((fp_in = fopen(argv[1],"r")) == NULL) {
+    perror("open failed on input");
+    return 1;
+  }
+  if ((fp_out = fopen("stereo.pcm","w")) == NULL) {
+    perror("open failed on output");
+    return 1;
+  }
+  signed short buf;
+  while (fread(&buf, sizeof (signed short), 1, fp_in) > 0) {
+    fwrite(&buf, sizeof (signed short), 1, fp_out);
+    if (ferror(fp_out)) {
+      perror("write failed\n");
+      return 1;
     }
   }
-
+  if (ferror(fp_in)) {
+    perror("read failed\n");
+    return 1;
+  }
+  fclose(fp_in);
+  fclose(fp_out);
   return 0;
 }
